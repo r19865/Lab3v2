ec859fbde560db96de311bb34ccfa321d7938256
Amy Telck <atelck@gmail.com>
2018-10-10 15:24:05 -0600

    Attempt

--- MyGITTest.java
+++ MyGITTest.java
@@ -3,6 +3,6 @@ public class MyGITTest
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Hello Reina, GIT Test Branch 2");
+		System.out.println("Hello Amy, GIT Test Branch 2");
 	}
 }
