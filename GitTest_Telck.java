22429765f2b3e8320ba699b8517ea6568849f8cc
Amy Telck <atelck@gmail.com>
2018-10-09 18:14:30 -0600

    Hellow message fix 2

--- GitTest_Telck.java
+++ GitTest_Telck.java
@@ -3,7 +3,7 @@ public class GitTest_Telck {
 
 	public static void main(String[] args) {
 
-		System.out.println("Amy Hello Git Test AAAA");
+		System.out.println("Amy Hello Branch2 Git Test");
 
 	}
 
